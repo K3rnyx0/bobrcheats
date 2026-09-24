6b7738142fc63944385076968c1f131bc83aa1d3
K3rnyx0 <gg_100gg_g@mail.ru>
2026-09-24 20:31:36 +0300

    Add files via upload

--- script.lua
+++ script.lua
@@ -1,4 +1,4 @@
--- bobrcheats v22.8 
+-- bobrcheats v23.3 
 -- Xeno Executor
 
 -- локализация: загружаем словарь из GitHub
@@ -1635,7 +1635,7 @@ local TitleText = Instance.new("TextLabel")
 TitleText.Size = UDim2.new(1, -60, 1, 0)
 TitleText.Position = UDim2.new(0, 16, 0, 0)
 TitleText.BackgroundTransparency = 1
-TitleText.Text = "bobrcheats v22.8"
+TitleText.Text = "bobrcheats v23.3"
 TitleText.TextColor3 = Settings.AccentColor
 TitleText.Font = Enum.Font.GothamBold
 TitleText.TextSize = 18
@@ -7663,7 +7663,7 @@ task.spawn(function()
     task.wait(0.5)
     LOAD.Gui:Destroy()
     ShowMainMenu()
-    print(Locales.t("[bobrcheats v22.8] Меню загружено."))
+    print(Locales.t("[bobrcheats v23.3] Меню загружено."))
 end)
 
 local ContextActionService = game:GetService("ContextActionService")
@@ -7700,4 +7700,4 @@ task.spawn(function()
 end)
 FeatureHealth.Start()
 
-print(Locales.t("[bobrcheats v22.8] Загрузка..."))
+print(Locales.t("[bobrcheats v23.3] Загрузка..."))
